de5f279e101f83f92eedd187b9d39c2ec6fd7e05
haroon-sidheekhi-vst-au4 <54521005+haroon-sidheekhi-vst-au4@users.noreply.github.com>
2020-03-18 12:26:29 +0530

    Update acronym.c

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,10 @@
 #include "acronym.h"
+#include <stdio.h>
+int main(){
+    char x[20],y[20],z[20];
+    printf("Enter the name that you want convert");
+    scanf("%s %s %s", x, y, z);
+    printf("The acronym is :");
+     printf("%c. %c. %s\n", x[0], y[0], z[0]);
+     return 0;
+}
